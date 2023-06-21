3cb21cb64da607b16c9760ec27c94a3f6daa1363
schoondw <schoondw@gmail.com>
2023-06-21 15:04:46 +0200

    Converted C3D_to_qtm to function, and related improvements/adaptations

--- theia_pose_conversion/Matlab/C3D_to_QTM_mat.m
+++ theia_pose_conversion/Matlab/C3D_to_QTM_mat.m
@@ -1,3 +1,4 @@
+function status = C3D_to_QTM_mat(varargin)
 % Convert C3D mat files to QTM skeleton
 %
 % Steps:
@@ -7,54 +8,35 @@
 % - Save QTM mat containing all skeletons to subfolder QTM_format
 
 %% Parameters
-admin_file = 'admin.xlsx';
-trial_sheet = 'trials';
-skel_sheet = 'skeletons';
+status = false;
 
-% theia_version_default = 'Theia3D 2021.2.0.1675'; % Default (used version before meta data was included in the visual3d export)
-% theia_version_default = 'Theia3D xxxx.x.x.xxxx';
-skel_base = 'pose_filt';
+admin_file_default = 'admin.xlsx';
+trial_sheet_default = 'trials';
+
+write_skeleton_info_default = true;
+skeleton_info_sheet_default = 'skeleton_info';
+
+theia_pose_base_default = 'pose_filt';
+qtm_format_output_suffix_default = '_theia_pose';
 
 rot_suffix = '_4X4';
 ignore_segments = {'worldbody'}; % List of rotation segments to ignore (without suffix)
 
-default_model = 'standard';
-animation_model = 'animation';
-animation_model_segments = {'abdomen','thorax','neck'};
+default_model_default = 'standard';
+animation_model_default = 'animation';
+animation_model_segments_default = {'abdomen','thorax','neck'};
 
-qtm_format_folder = 'qtm_format';
+verbose_default = true;
 
-admin_only = false; % Only adds info to admin/skip writing mat file when true
-verbose = true;
-
-%% Read admin
-trial_tab = readtable(admin_file,'Sheet',trial_sheet);
-n_trials = height(trial_tab);
-
-string_vars = {'theia_version'};
-if sum(ismember(trial_tab.Properties.VariableNames,string_vars))>0
-    trial_tab = convertvars(trial_tab,string_vars,'string');
-end
-
-project_path = pwd;
-
-% Project name
-i_filesep = strfind(project_path,filesep);
-project_name = project_path(i_filesep(end)+1:end);
-
-
-%% Initiate skeleton table
-
-% Output variable definitions: name and type
-% Columns (from admin): subject_folder, session_folder, data_folder, trial
-% New columns: n_frames, frame_rate,
-%   skel_id, fill_level_av, fill_level_sd, fill_level_min, fill_level_max
-skelVarDef = {... 
-    'subject_folder','string';...
-    'session_folder','string';...
-    'data_folder','string';...
+trialVarDef_default = {... 'subject_folder','string';... 'session_folder','string';... 'qtm_project_name','string';... 'qtm_trial_path','string';...
+    'theia_data_path','string';...
+    'qtm_data_path','string';...
+    'qtm_format_output_path','string';...    'qtm_format_output_suffix','string';...
     'trial','string';...
-    'processing_date','string';...
+    'n_skel','double';...
+    'processing_date','string'};
+
+skelVarDef_default = {... 
     'theia_version','string';...
     'model','string';...
     'n_frames','double';...
@@ -66,14 +48,51 @@ skelVarDef = {...
     'fill_level_min','double';...
     'fill_level_max','double'...
     };
-n_vars=size(skelVarDef,1);
 
-max_rows = n_trials*10;
+% Fixed parameters
+admin_only = false; % Only adds info to admin/skip writing mat file when true (for debugging)
 
-% Initialize table
-skel_tab = table('Size',[max_rows n_vars],...
-    'VariableTypes',skelVarDef(:,2),'VariableNames',skelVarDef(:,1));
+%% Parse input
+p = inputParser;
+p.KeepUnmatched = true;
 
+validScalarPosInt = @(x) isnumeric(x) && isscalar(x) && (x > 0) && x==floor(x);
+istext = @(x) isstring(x) || ischar(x);
+
+addParameter(p,'admin_file', admin_file_default, istext);
+addParameter(p,'trial_sheet', trial_sheet_default, istext);
+addParameter(p,'write_skeleton_info', write_skeleton_info_default, @islogical);
+addParameter(p,'skeleton_info_sheet', skeleton_info_sheet_default, istext);
+addParameter(p,'theia_pose_base', theia_pose_base_default, istext);
+addParameter(p,'qtm_format_output_suffix', qtm_format_output_suffix_default, istext);
+addParameter(p,'default_model', default_model_default, istext);
+addParameter(p,'animation_model', animation_model_default, istext);
+addParameter(p,'animation_model_segments', animation_model_segments_default, @iscell);
+addParameter(p,'verbose', verbose_default, @islogical);
+addParameter(p,'trialVarDef', trialVarDef_default, @iscell);
+addParameter(p,'skelVarDef', skelVarDef_default, @iscell);
+
+parse(p,varargin{:});
+
+Opts = p.Results;
+
+%% Read admin
+trial_tab = readtable(Opts.admin_file, 'Sheet', Opts.trial_sheet);
+n_trials = height(trial_tab);
+
+%% Initiate skeleton table
+
+% Output variable definitions: name and type
+if Opts.write_skeleton_info
+    skelVarDef = [Opts.trialVarDef; Opts.skelVarDef];
+    n_vars=size(skelVarDef,1);
+    
+    n_rows = sum(trial_tab{:,'n_skel'}); % n_trials*10;
+    
+    % Initialize table
+    skel_tab = table('Size',[n_rows n_vars],...
+        'VariableTypes',skelVarDef(:,2),'VariableNames',skelVarDef(:,1));
+end
 
 %% Loop per trial (row in admin)
 
@@ -88,27 +107,17 @@ for i_trial = 1:n_trials
     end
     
     trial_name = char(trial_tab{i_trial,'trial'});
+    trial_pose_output = [trial_name, Opts.qtm_format_output_suffix];
+    pn_theia = char(trial_tab{i_trial,'theia_data_path'});
+    pn_output = char(trial_tab{i_trial,'qtm_format_output_path'});
     
-    % Trial path
-    fn_partial_qtm = fullfile(project_name,'Data',...
-        char(trial_tab{i_trial,'subject_folder'}),...
-        char(trial_tab{i_trial,'session_folder'})...
-        );
-    
-    fn = fullfile(project_path,'Data',...
-        char(trial_tab{i_trial,'subject_folder'}),...
-        char(trial_tab{i_trial,'session_folder'}),...
-        char(trial_tab{i_trial,'data_folder'}),...
-        char(trial_tab{i_trial,'trial'})...
-        );
-    
-    if verbose
-        fprintf('- Processing trial %d/%d: %s\n', i_trial, n_trials, fn);
+    if Opts.verbose
+        fprintf('- Processing trial %d/%d: %s\n', i_trial, n_trials, pn_theia);
     end
     
     % Initiate QTM data structure (Matlab export format)
     qtm = struct(...
-        'File',fullfile(fn_partial_qtm, [trial_name, '.qtm']),...
+        'File',fullfile(trial_tab{i_trial,'qtm_data_path'},[trial_name, '.qtm']),... % Reference to original QTM file (reconstructed)
         'Timestamp',[],...
         'StartFrame',1,...
         'Frames',[],...
@@ -116,14 +125,14 @@ for i_trial = 1:n_trials
         'Skeletons',struct([]));
     
     for i_skel = 1:n_skel
-        skel_name = sprintf('%s_%d', skel_base, i_skel-1);
+        skel_name = sprintf('%s_%d', Opts.theia_pose_base, i_skel-1);
         
-        if verbose
+        if Opts.verbose
             fprintf('-- Skel: %s (%d/%d)\n', skel_name, i_skel, n_skel);
         end
         
         % Read skeleton file and extract info
-        c3d = ezc3dRead(fullfile(fn, [skel_name, '.c3d']));
+        c3d = ezc3dRead(fullfile(pn_theia, [skel_name, '.c3d']));
         
         % Check requirements
         if c3d.parameters.ROTATION.USED.DATA < 1 % Number of segments
@@ -141,7 +150,8 @@ for i_trial = 1:n_trials
         % frame_rate = S.FRAME_RATE{1};
         frame_rate = c3d.parameters.ROTATION.RATE.DATA;
         
-        theia_version = sprintf('Theia3D %d.%d.%d.%d',c3d.parameters.THEIA3D.THEIA3D_VERSION.DATA);
+        theia_version = sprintf('Theia3D %d.%d.%d.%d',...
+            c3d.parameters.THEIA3D.THEIA3D_VERSION.DATA);
         
         if i_skel == 1
             qtm.Frames = n_frames;
@@ -158,17 +168,15 @@ for i_trial = 1:n_trials
         segment_labels = segment_labels(segm_idx);
         
         % Convert to QTM position and rotation multidim matrix representation (rigid body)
-        % pos = nan(3,n_segments,n_frames);
-        % rot = nan(9,n_segments,n_frames);
         pos = reshape(c3d.data.rotations(1:3,4,segm_idx,:),...
             3,n_segments,n_frames);
         rot = reshape(c3d.data.rotations(1:3,1:3,segm_idx,:),...
             9,n_segments,n_frames);
         
         % Infer model used
-        model = default_model;
-        if prod(ismember(animation_model_segments,segment_labels))==1
-            model = animation_model;
+        model = Opts.default_model;
+        if prod(ismember(Opts.animation_model_segments, segment_labels))==1
+            model = Opts.animation_model;
         end
         
         % QTM rigid body-like structure
@@ -193,39 +201,37 @@ for i_trial = 1:n_trials
         % Add skeleton structure to qtm structure
         qtm.Skeletons = [qtm.Skeletons qtm_skel];
         
-        % Extract fill levels per segment
-        segm_fill_perc = sum(~isnan(squeeze(pos(1,:,:))),2)./n_frames*100;
-        
-        % Add row to skel_tab
-        row_counter = row_counter+1;
-        
-        skel_tab(row_counter,'subject_folder') = trial_tab(i_trial,'subject_folder');
-        skel_tab(row_counter,'session_folder') = trial_tab(i_trial,'session_folder');
-        skel_tab(row_counter,'data_folder') = trial_tab(i_trial,'data_folder');
-        skel_tab{row_counter,'trial'} = string(trial_name);
-        skel_tab(row_counter,'processing_date') = trial_tab(i_trial,'processing_date');
-        skel_tab{row_counter,'theia_version'} = string(theia_version);
-        skel_tab{row_counter,'model'} = string(model);
-        skel_tab{row_counter,'n_frames'} = n_frames;
-        skel_tab{row_counter,'frame_rate'} = frame_rate;
-        skel_tab{row_counter,'skel_id'} = string(skel_name);
-        skel_tab{row_counter,'n_segments'} = n_segments;
-        skel_tab{row_counter,'fill_level_av'} = mean(segm_fill_perc);
-        skel_tab{row_counter,'fill_level_sd'} = std(segm_fill_perc);
-        skel_tab{row_counter,'fill_level_min'} = min(segm_fill_perc);
-        skel_tab{row_counter,'fill_level_max'} = max(segm_fill_perc);
-        
+        if Opts.write_skeleton_info
+            % Extract fill levels per segment
+            segm_fill_perc = sum(~isnan(squeeze(pos(1,:,:))),2)./n_frames*100;
+            
+            % Add row to skel_tab
+            row_counter = row_counter+1;
+            
+            skel_tab(row_counter,'theia_data_path') = trial_tab(i_trial,'theia_data_path');
+            skel_tab(row_counter,'qtm_data_path') = trial_tab(i_trial,'qtm_data_path');
+            skel_tab(row_counter,'qtm_format_output_path') = trial_tab(i_trial,'qtm_format_output_path');
+            skel_tab{row_counter,'trial'} = string(trial_name);
+            skel_tab(row_counter,'processing_date') = trial_tab(i_trial,'processing_date');
+            skel_tab{row_counter,'theia_version'} = string(theia_version);
+            skel_tab{row_counter,'model'} = string(model);
+            skel_tab{row_counter,'n_frames'} = n_frames;
+            skel_tab{row_counter,'frame_rate'} = frame_rate;
+            skel_tab{row_counter,'skel_id'} = string(skel_name);
+            skel_tab{row_counter,'n_segments'} = n_segments;
+            skel_tab{row_counter,'fill_level_av'} = mean(segm_fill_perc);
+            skel_tab{row_counter,'fill_level_sd'} = std(segm_fill_perc);
+            skel_tab{row_counter,'fill_level_min'} = min(segm_fill_perc);
+            skel_tab{row_counter,'fill_level_max'} = max(segm_fill_perc);
+        end
     end
-    
-    % Add Theia version info to trial tab
-    trial_tab{i_trial,'theia_version'} = string(theia_version);
     
     % Save QTM mat file containing all skeletons
     if ~admin_only
-        if ~exist(fullfile(fn,qtm_format_folder),'dir')
-            mkdir(fn,qtm_format_folder);
+        if ~exist(pn_output,'dir')
+            mkdir(pn_output);
         end
-        save(fullfile(fn,qtm_format_folder, [trial_name, '.mat']), 'qtm');
+        save(fullfile(pn_output, [trial_pose_output, '.mat']), 'qtm');
     end
     
 end
@@ -233,15 +239,14 @@ end
 
 %% Write skeleton tab to Excel
 
-% Update trial tab (added Theia version info)
-writetable(trial_tab,admin_file,...
-    'Sheet',trial_sheet,'WriteMode','overwritesheet');
-
 % Add/replace skeleton tab
-writetable(skel_tab(1:row_counter,:),admin_file,...
-    'Sheet',skel_sheet,'WriteMode','overwritesheet');
+if Opts.write_skeleton_info
+    writetable(skel_tab(1:row_counter,:), Opts.admin_file,...
+        'Sheet',Opts.skeleton_info_sheet, 'WriteMode','overwritesheet');
+end
+status = true;
 
-if verbose
-    disp('Done!')
+if Opts.verbose
+    disp('Theia pose conversion to QTM MAT format done!')
 end
 
--- theia_pose_conversion/Matlab/Create_project_admin.m
+++ theia_pose_conversion/Matlab/Create_project_admin.m
@@ -9,27 +9,31 @@ function status = Create_project_admin(varargin)
 % - Columns: subject_folder, session_folder, data_folder, trial, n_skel
 % - Save to admin.xlsx, sheet: trials
 
-if ~isfolder('Data')
-    error('Current path is not a valid project folder.')
-end
-
 %% Parameters
 status = false;
 
 % - Admin info
+qtm_project_root_check_default = true;
 admin_file_default = 'admin.xlsx';
-data_folder_default = 'Data';
-theia_output_folder_default = 'TheiaFormatData';
-theia_pose_base_default = 'pose_filt';
 trial_sheet_default = 'trials';
-max_rows_default = 1000;
+theia_output_folder_default = 'TheiaFormatData'; % subfolder of qtm trial path
+qtm_data_folder_rel_theia_output_folder_default = '..';
+qtm_format_output_folder_default = {'theia_data_path','qtm_format'};
+theia_pose_base_default = 'pose_filt';
+max_trials_default = 1000;
 verbose_default = true;
 
-trialVarDef_default = {{... 'subject_folder','string';... 'session_folder','string';...
-        'data_folder','string';...
-        'trial','string';...
-        'n_skel','double';...
-        'processing_date','string'}};
+trialVarDef_default = {... 'subject_folder','string';... 'session_folder','string';... 'qtm_project_name','string';... 'qtm_trial_path','string';...
+    'theia_data_path','string';...
+    'qtm_data_path','string';...
+    'qtm_format_output_path','string';...    'qtm_format_output_suffix','string';...
+    'trial','string';...
+    'n_skel','double';...
+    'processing_date','string'};
+
+    
+% Fixed parameters
+qtm_data_folder = 'Data';
 
 %% Parse input arguments
 p = inputParser;
@@ -38,12 +42,15 @@ p.KeepUnmatched = true;
 validScalarPosInt = @(x) isnumeric(x) && isscalar(x) && (x > 0) && x==floor(x);
 istext = @(x) isstring(x) || ischar(x);
 
+addParameter(p,'qtm_project_root_check', qtm_project_root_check_default, @islogical);
 addParameter(p,'admin_file', admin_file_default, istext);
-addParameter(p,'data_folder', data_folder_default, istext);
-addParameter(p,'theia_output_folder', theia_output_folder_default, istext);
-addParameter(p,'theia_pose_base', theia_pose_base_default, istext);
 addParameter(p,'trial_sheet', trial_sheet_default, istext);
-addParameter(p,'max_rows', max_rows_default, validScalarPosInt);
+addParameter(p,'theia_output_folder', theia_output_folder_default, istext);
+addParameter(p,'qtm_data_folder_rel_theia_output_folder', ...
+    qtm_data_folder_rel_theia_output_folder_default, istext);
+addParameter(p,'qtm_format_output_folder', qtm_format_output_folder_default, @iscell);
+addParameter(p,'theia_pose_base', theia_pose_base_default, istext);
+addParameter(p,'max_trials', max_trials_default, validScalarPosInt);
 addParameter(p,'verbose', verbose_default, @islogical);
 addParameter(p,'trialVarDef', trialVarDef_default, @iscell);
 
@@ -52,23 +59,30 @@ parse(p,varargin{:});
 Opts = p.Results;
 
 %% Identify Theia output folders (subfolders of current dir)
+if Opts.qtm_project_root_check
+    % Require that current folder contains the QTM data folder (weak requirement for a project)
+    if ~isfolder(qtm_data_folder)
+        disp('Current folder should be QTM project root.')
+        return
+    end
+end
+
 C = textscan(genpath('.'),'%s','Delimiter',';');
 idx = endsWith(C{1}, Opts.theia_output_folder);
-
 if sum(idx) == 0
     disp('No Theia output folders found on current path.')
     return
 end
 
-datafolder_list = C{1}(idx);
-n_datafolders = length(datafolder_list);
+theia_datafolder_list = C{1}(idx);
+n_datafolders = length(theia_datafolder_list);
 
 %% Initiate trial table
 
 n_vars=size(Opts.trialVarDef,1);
 
 % Initialize table
-trial_tab = table('Size',[Opts.max_rows n_vars],...
+trial_tab = table('Size',[Opts.max_trials n_vars],...
     'VariableTypes',Opts.trialVarDef(:,2),'VariableNames',Opts.trialVarDef(:,1));
 
 %% Loop through folder structure
@@ -76,7 +90,10 @@ trial_tab = table('Size',[Opts.max_rows n_vars],...
 row_counter = 0; % Counter for total number of trials
 
 for i_df = 1:n_datafolders
-    current_datafolder = datafolder_list{i_df};
+    current_datafolder = theia_datafolder_list{i_df};
+    
+    qtm_data_path = fullfile(current_datafolder, ...
+        Opts.qtm_data_folder_rel_theia_output_folder);
     
     % Identify trial folders that may contain data
     D = dir(current_datafolder);
@@ -97,11 +114,19 @@ for i_df = 1:n_datafolders
             % Write row
             row_counter = row_counter+1;
             
-            trial_tab{row_counter,'data_folder'} = string(current_datafolder);
+            trial_tab{row_counter,'theia_data_path'} = ...
+                string(fullfile(current_datafolder, current_trial));
+            trial_tab{row_counter,'qtm_data_path'} = string(qtm_data_path);
             trial_tab{row_counter,'trial'} = string(current_trial);
             trial_tab{row_counter,'n_skel'} = n_skel;
             
             trial_tab{row_counter,'processing_date'} = string(D(1).date);
+            
+            % Define and add QTM format output dir
+            base_folder = trial_tab{row_counter, Opts.qtm_format_output_folder{1}}; % qtm_/theia_data_path
+            qtm_format_output_path = fullfile(base_folder, Opts.qtm_format_output_folder{2});
+            trial_tab{row_counter,'qtm_format_output_path'} = string(qtm_format_output_path);
+            % trial_tab{row_counter,'qtm_format_output_suffix'} = string(Opts.qtm_format_output_suffix);
         end
     end
 end
