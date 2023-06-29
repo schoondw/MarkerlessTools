a718cd3c8c22ba9c5d0b4d194986a8208bb790b1
schoondw <schoondw@gmail.com>
2023-06-29 10:41:38 +0200

    Added Matlab help info

---
+++ MarkerlessTools.m
@@ -0,0 +1,19 @@
+% MarkerlessTools
+% 
+% Toolbox for processing and analysis of markerless mocap data
+% 
+% Main folders:
+% - py_theia_batch: Python tools for batch processing with Theia
+% - theia_pose_conversion: Matlab tools for conversion of Theia pose data
+%   to QTM-like export formats
+% 
+% Main scripts:
+% - theia_pose_conversion\markerless_data_conversion_main_script.m
+% - theia_pose_conversion\markerless_meta_data_main_script.m
+% 
+% Requirements:
+% - QTMTools: https://github.com/schoondw/QTMTools
+% - Statistics and Machine Learning Toolbox: https://mathworks.com/products/statistics.html
+%   (only required for Theia processing statistics)
+% 
+% Author: Erwin Schoonderwaldt
